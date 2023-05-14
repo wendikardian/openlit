efdcf49b45394d187fad0fc926f29badf06be22d
Wendi Kardian <69452468+wendikardian@users.noreply.github.com>
2023-05-14 12:48:06 +0700

    updated

--- src/virtual_assist/Assistant.jsx
+++ src/virtual_assist/Assistant.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
-import ReusableHeader from '../components/ReusableHeader'
+import React from "react";
+import ReusableHeader from "../components/ReusableHeader";
+import "./style.css";
+import { Input } from "antd";
+
+const { Search } = Input;
 
 export default function Assistant() {
   return (
     <div>
-        <ReusableHeader />
-        <h1 >Assistant</h1>
-        <h1 >Assistant</h1>
-        <h1 >Assistant</h1>
-        <h1 >Assistant</h1>
-        <h1 >Assistant</h1>
-        <h1 >Assistant</h1>
-        <h1 >Assistant</h1>
+      <ReusableHeader />
+      <div className="virtual-content">
+        <h1 className="class-title">Assistant</h1>
+        <div className="text-form">
+          <Search
+            placeholder="input search text"
+            allowClear
+            enterButton="Send"
+            size="large"
+            style={{backgroundColor: "#06C2FA"}}
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
